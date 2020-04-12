630cc7bd43a4898b1c51f9924bf174746e6472f9
baoyachi <liaoymxsdl@gmail.com>
2020-04-12 20:39:08 +0800

    update ip mask

--- src/ip.rs
+++ src/ip.rs
@@ -23,7 +23,7 @@ pub fn mask(input: &str) -> nom::IResult<&str, &str> {
 }
 
 pub fn ip_mask(input: &str) -> nom::IResult<&str, (&str, &str)> {
-    let split = alt((tag("/"), tag("-"), tag(" ")));
+    let split = alt((tag("/"), tag("-"), tag(" "),tag("~")));
     let (input, (ip, _, mask)) = tuple((ip, split, mask))(input)?;
     Ok((input, (ip, mask)))
 }
@@ -47,6 +47,11 @@ mod tests {
         assert_eq!(mask, "255.255.0.0");
 
         let (input, (ip, mask)) = ip_mask("127.0.0.1 255.255.0.0").unwrap();
+        assert_eq!(input, "");
+        assert_eq!(ip, "127.0.0.1");
+        assert_eq!(mask, "255.255.0.0");
+
+        let (input, (ip, mask)) = ip_mask("127.0.0.1~255.255.0.0").unwrap();
         assert_eq!(input, "");
         assert_eq!(ip, "127.0.0.1");
         assert_eq!(mask, "255.255.0.0");
